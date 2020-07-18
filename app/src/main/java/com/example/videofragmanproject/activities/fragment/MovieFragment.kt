4b3eb538abab0acd18369a8b803fb881b3b279ad
Burakcan <brkcnszgn@gmail.com>
2020-07-18 15:47:57 +0300

    class Tanimlamalari

--- app/src/main/java/com/example/videofragmanproject/activities/fragment/MovieFragment.kt
+++ app/src/main/java/com/example/videofragmanproject/activities/fragment/MovieFragment.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.videofragmanproject.R
 import com.example.videofragmanproject.adapter.FragmanAdapter
+import com.example.videofragmanproject.databinding.FragmentMovieBinding
 import com.example.videofragmanproject.mock.MockData
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.ExoPlayerFactory
@@ -19,38 +20,40 @@ import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.bottom_sheet.*
 import kotlinx.android.synthetic.main.fragment_movie.*
+import kotlinx.android.synthetic.main.fragment_movie.bottom_sheet
 
 
 class MovieFragment : Fragment() {
 
+    private lateinit var binding: FragmentMovieBinding
     private lateinit var simpleExoPlayer: SimpleExoPlayer
-
-    // exoPlayer'da kullanmak icin DataSource nesnesi tanımı
     private lateinit var mediaDataSourceFactory: DataSource.Factory
-
-    // exoPlayer'da kullanılacak olan url
     val URL = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_movie, container, false)
+        binding = FragmentMovieBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        videoOynat(URL)
-        recycle_frg.adapter = FragmanAdapter(MockData.getFragmanList()) {
 
-            //startActivity(Intent(view.context, FragmanDetailActivity::class.java))
+        videoOynat(URL)
+        binding.recycleFrg.adapter = FragmanAdapter(MockData.getFragmanList()) {
             videoOynat(it.videoUrl)
         }
 
-
     }
+
 
     open fun videoOynat(url: String) {
         // yeni bir instance baslatılması
@@ -74,13 +77,55 @@ class MovieFragment : Fragment() {
         simpleExoPlayer.playWhenReady = true
 
         // loyout dosyasındaki id degeri eslestirme
-        player_view.player = simpleExoPlayer
+        binding.playerView.player = simpleExoPlayer
 
         // player ekranına focuslanma ozelligi
         // player_view.requestFocus()
-        player_view.useController = false
-       /* player_view.isSoundEffectsEnabled = false
-        player_view.player.audioComponent!!.volume = 0f*/
+        binding.playerView.useController = false
+        //setBottomSheet()
     }
+
+    fun clickFunction(buttonSheetBehavior: BottomSheetBehavior<*>) {
+        layoutSheet.setOnClickListener {
+            when (buttonSheetBehavior.state) {
+                BottomSheetBehavior.STATE_COLLAPSED -> buttonSheetBehavior.setState(
+                    BottomSheetBehavior.STATE_EXPANDED
+                )
+                BottomSheetBehavior.STATE_EXPANDED -> buttonSheetBehavior.setState(
+                    BottomSheetBehavior.STATE_COLLAPSED
+                )
+            }
+        }
+
+    }
+
+    private fun setBottomSheet() {
+        val bottomSheetBehavior: BottomSheetBehavior<*> = BottomSheetBehavior.from(bottom_sheet)
+        clickFunction(bottomSheetBehavior)
+        //bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        //bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        bottomSheetBehavior.isHideable = false
+        bottomSheetBehavior.setBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> imageBottomSheet.setImageResource(R.mipmap.ic_launcher)
+                    BottomSheetBehavior.STATE_EXPANDED -> imageBottomSheet.setImageResource(R.mipmap.ic_launcher)
+                    BottomSheetBehavior.STATE_DRAGGING -> imageBottomSheet.setImageResource(R.mipmap.ic_launcher)
+                }
+                println("state ----->>>>>>>>>>>>> $newState")
+            }
+
+            override fun onSlide(
+                bottomSheet: View,
+                slideOffset: Float
+            ) {
+                //  imageBottomSheet.setImageResource(R.mipmap.icon_bottom_sheet_default);
+                println("slide ----->>>>>>>>>>>>> ")
+            }
+        })
+    }
+
 
 }
